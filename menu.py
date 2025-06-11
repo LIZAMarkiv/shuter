abc1294f2904e1f6b2961877cf11ba8ece3b6d89
bubna92 <lizamarkiv219@gmail.com>
2025-06-11 14:24:26 +0300

    main

---
+++ menu.py
@@ -0,0 +1,22 @@
+from PyQt6.QtWidgets import*
+from main import*
+
+app = QApplication([])
+window = QWidget()
+knopka1 = QPushButton("start")
+knopka2 = QPushButton("shop")
+knopka3 = QPushButton("setting")
+knopka4 = QPushButton("exit")
+
+main_lain = QVBoxLayout()
+main_lain.addWidget(knopka1)
+main_lain.addWidget(knopka2)
+main_lain.addWidget(knopka3)
+main_lain.addWidget(knopka4)
+
+
+window.setLayout(main_lain)
+knopka1.clicked.connect(start_game)
+
+window.show()
+app.exec()
